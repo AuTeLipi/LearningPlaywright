104880246cec67de10ce23a77d982224adae7983
Student <student@example.com>
2026-02-17 15:36:09 +0530

    Update Chapter 02 JS Concepts

---
+++ Chapter_02_JS_Concepts/04_Core_Comments_JS.js
@@ -0,0 +1,17 @@
+// This is a comment - this code will not executed
+
+/** 
+ * This is multi line comment 
+ * Author : Lipi Dubbaka
+ * Date : 17-Feb-2026
+ * Purpose : Learning JS Comments
+ **/
+
+/* 
+ * This is multi line comment 
+ * Author : Lipi Dubbaka
+ * Date : 17-Feb-2026
+ * Purpose : Learning JS Comments
+ */
+
+console.log("I am learning JS Comments");
